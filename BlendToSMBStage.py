4ab7d37cfb82d950488eb5401cf2f0038e162738
CraftedCart <CraftedCart@users.noreply.github.com>
2018-01-31 02:57:17 +0000

    Got mostly working blend -> ws2 config exporter

--- BlendToSMBStage.py
+++ BlendToSMBStage.py
@@ -1,7 +1,11 @@
 import bpy
-# from lxml import etree
-import xml.etree.ElementTree as etree
+from sys import platform
 import math
+
+if platform == "linux" or platform == "linux2":
+    from lxml import etree
+else:
+    import xml.etree.ElementTree as etree
 
 bl_info = {
     "name": "BlendToSMBStage",
@@ -15,6 +19,21 @@ bl_info = {
     "tracker_url": "",
     "category": "Object"
 }
+
+def setItemGroupProperties(ig, animated = False):
+    ig["collisionStartX"] = -256
+    ig["collisionStartY"] = -256
+    ig["collisionStepX"] = 32
+    ig["collisionStepY"] = 32
+    ig["collisionStepCountX"] = 16
+    ig["collisionStepCountY"] = 16
+
+    ig["posXAnim"] = animated
+    ig["posYAnim"] = animated
+    ig["posZAnim"] = animated
+    ig["rotXAnim"] = animated
+    ig["rotYAnim"] = animated
+    ig["rotZAnim"] = animated
 
 def addPosXAnim(parent):
     startFrame = bpy.context.scene.frame_start
@@ -98,7 +117,7 @@ def addPosZAnim(parent):
     endFrame = bpy.context.scene.frame_end
 
     bpy.context.scene.frame_set(0)
-    initVal = bpy.context.scene.objects.active.location.z
+    initVal = -bpy.context.scene.objects.active.location.z
 
     hitLast = False;
 
@@ -253,12 +272,38 @@ class NewItemGroup(bpy.types.Operator):
         ig = bpy.data.objects.new("[IG] New item group", None)
         bpy.context.scene.objects.link(ig)
         ig.empty_draw_type = "ARROWS"
-        ig["collisionStartX"] = -256
-        ig["collisionStartY"] = -256
-        ig["collisionStepX"] = 32
-        ig["collisionStepY"] = 32
-        ig["collisionStepCountX"] = 16
-        ig["collisionStepCountY"] = 16
+        setItemGroupProperties(ig)
+
+        return {'FINISHED'}
+
+#Operation
+class NewAnimatedItemGroup(bpy.types.Operator):
+    bl_idname = "object.new_animated_item_group"
+    bl_label = "New animated item group"
+    bl_description = "Creates an empty with various custom properties\nParent your object to the empty to add it to the item group"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    #Execute function
+    def execute(self, context):
+        ig = bpy.data.objects.new("[IG] New item group", None)
+        bpy.context.scene.objects.link(ig)
+        ig.empty_draw_type = "ARROWS"
+        setItemGroupProperties(ig, True)
+
+        return {'FINISHED'}
+
+#Operation
+class ConvertToAnimtedGroup(bpy.types.Operator):
+    bl_idname = "object.convert_to_animated_group"
+    bl_label = "Convert active to animated item group"
+    bl_description = "Creates an empty with various custom properties\nParent your object to the empty to add it to the item group"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    #Execute function
+    def execute(self, context):
+        obj = bpy.context.scene.objects.active
+        setItemGroupProperties(obj, True)
+        obj.name = "[IG] " + obj.name
 
         return {'FINISHED'}
 
@@ -300,8 +345,101 @@ class CopyAnimXML(bpy.types.Operator):
             kf.append(rotZ)
 
         #Copy the XML to the clipboard
-        # bpy.context.window_manager.clipboard = str(etree.tostring(kf, pretty_print = True, encoding = "unicode"))
-        bpy.context.window_manager.clipboard = str(etree.tostring(kf, encoding = "unicode"))
+        if platform == "linux" or platform == "linux2":
+            bpy.context.window_manager.clipboard = str(etree.tostring(kf, pretty_print = True, encoding = "unicode"))
+        else:
+            bpy.context.window_manager.clipboard = str(etree.tostring(kf, encoding = "unicode"))
+
+        return {'FINISHED'}
+
+#Operation
+class GenerateConfig(bpy.types.Operator):
+    bl_idname = "object.generate_config"
+    bl_label = "Generate config to clipboard"
+    bl_description = "Generates a config from this scene"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    #Execute function
+    def execute(self, context):
+        root = etree.Element("superMonkeyBallStage", version="1.1.0")
+        modelImport = etree.SubElement(root, "modelImport")
+        modelImport.text = "//PUT A PATH TO YOUR OBJ HERE"
+
+        start = etree.SubElement(root, "start")
+        etree.SubElement(start, "position", x = "0", y = "0", z = "0")
+        etree.SubElement(start, "rotation", x = "0", y = "0", z = "0")
+
+        etree.SubElement(root, "falloutPlane", y = "-10")
+
+        itemGroups = [obj for obj in bpy.context.scene.objects if obj.name.startswith("[IG]")]
+
+        for ig in itemGroups:
+            print("Processing item group: " + ig.name)
+
+            #XML Item Group
+            xig = etree.SubElement(root, "itemGroup")
+
+            igName = etree.SubElement(xig, "name")
+            igName.text = ig.name
+
+            # etree.SubElement(xig, "rotationCenter", x = str(ig.location.x), y = str(ig.location.y), z = str(ig.location.z))
+            etree.SubElement(xig, "initialRotation", x = "0", y = "0", z = "0")
+            animType = etree.SubElement(xig, "animSeesawType")
+            animType.text = "LOOPING_ANIMATION"
+            animLoopTime = etree.SubElement(xig, "animLoopTime") #TODO: Allow different loop times per item group
+            animLoopTime.text = str(bpy.context.scene.frame_end / 60.0)
+
+            #Collision grid #TODO: Adjustible collision grid
+            grid = etree.SubElement(xig, "collisionGrid")
+            etree.SubElement(grid, "start", x = "-256", z = "-256")
+            etree.SubElement(grid, "step", x = "32", z = "32")
+            etree.SubElement(grid, "count", x = "16", z = "16")
+
+            #Find all level models who are children of this item group
+            children = [ob_child for ob_child in bpy.context.scene.objects if ob_child.parent == ig]
+
+            #The item group should be added as a level model if it's not an empty
+            if ig.data != None:
+                children.append(ig)
+
+            for child in children:
+                print("    levelModel: " + child.name)
+                if child.name == child.data.name:
+                    model = etree.SubElement(xig, "levelModel")
+                    model.text = child.name.replace(" ", "_")
+                else:
+                    model = etree.SubElement(xig, "levelModel")
+                    model.text = (child.name + "_" + child.data.name).replace(" ", "_")
+
+            #Set this as the active (Required for the addPos/Rot anim, because I'm a lazy sod who can't be bothered to tweak it)
+            bpy.context.scene.objects.active = ig
+
+            animKeyframes = etree.SubElement(xig, "animKeyframes")
+            #Add animaton if requested
+            if "posXAnim" in ig and ig["posXAnim"] != 0:
+                posX = etree.SubElement(animKeyframes, "posX")
+                addPosXAnim(posX)
+            if "posYAnim" in ig and ig["posYAnim"] != 0:
+                posZ = etree.SubElement(animKeyframes, "posZ")
+                addPosYAnim(posZ)
+            if "posZAnim" in ig and ig["posZAnim"] != 0:
+                posY = etree.SubElement(animKeyframes, "posY")
+                addPosZAnim(posY)
+            if "rotXAnim" in ig and ig["rotXAnim"] != 0:
+                rotX = etree.SubElement(animKeyframes, "rotX")
+                addRotXAnim(rotX)
+            if "rotYAnim" in ig and ig["rotYAnim"] != 0:
+                rotZ = etree.SubElement(animKeyframes, "rotZ")
+                addRotYAnim(rotZ)
+            if "rotZAnim" in ig and ig["rotZAnim"] != 0:
+                rotY = etree.SubElement(animKeyframes, "rotY")
+                addRotZAnim(rotY)
+
+        #Copy the XML to the clipboard
+        if platform == "linux" or platform == "linux2":
+            bpy.context.window_manager.clipboard = str(etree.tostring(root, pretty_print = True, encoding = "unicode"))
+        else:
+            bpy.context.window_manager.clipboard = str(etree.tostring(root, encoding = "unicode"))
 
         return {'FINISHED'}
 
@@ -326,6 +464,9 @@ class BlendToSMBStagePanel(bpy.types.Panel):
         layout.label("Parent objects to the item group to add them")
         layout.label("Existing animated objects can just have the [IG] prefix applied to their name")
         layout.operator(NewItemGroup.bl_idname)
+        layout.operator(NewAnimatedItemGroup.bl_idname)
+
+        layout.operator(ConvertToAnimtedGroup.bl_idname)
 
         layout.prop(scene, "genPosXKeyframesProp")
         layout.prop(scene, "genPosYKeyframesProp")
@@ -342,6 +483,8 @@ class BlendToSMBStagePanel(bpy.types.Panel):
         layout.label("Rotations are always rounded to the nearest int")
 
         layout.operator(CopyAnimXML.bl_idname)
+
+        layout.operator(GenerateConfig.bl_idname)
 
 def register():
     bpy.utils.register_module(__name__)
